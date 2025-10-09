7f2137e37abce715a0ded2d3c1187fc67d22faa6
Ray Andrew <rs@rs.ht>
2025-10-09 01:28:16 -0500

    fix the chunking

--- src/dftracer/utils/bin/dftracer_split.cpp
+++ src/dftracer/utils/bin/dftracer_split.cpp
@@ -272,12 +272,10 @@ static std::vector<ChunkData> create_chunk_mappings(
                     std::floor(available_space / file.size_per_line));
             }
 
-            std::size_t events_to_take;
-            if (events_that_fit > 0 && events_that_fit < remaining_events) {
-                events_to_take = events_that_fit;
-            } else {
-                events_to_take = remaining_events;
-            }
+            // Always respect chunk size limit
+            std::size_t events_to_take = (events_that_fit > 0)
+                                         ? std::min(remaining_events, events_that_fit)
+                                         : remaining_events;
 
             if (events_to_take == 0 && remaining_events > 0) {
                 if (!current_chunk.specs.empty()) {
